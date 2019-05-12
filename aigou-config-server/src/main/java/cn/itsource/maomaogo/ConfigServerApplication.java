93e9d93e559165951e7d9ef1043240832071859f
lixuesong <admin>
2019-05-12 22:08:02 +0800

    测试git

--- aigou-config-server/src/main/java/cn/itsource/maomaogo/ConfigServerApplication.java
+++ aigou-config-server/src/main/java/cn/itsource/maomaogo/ConfigServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 测试 git是否安装成功
+ */
 @SpringBootApplication
 @EnableConfigServer // 表示这是一个 配置中心服务器
 public class ConfigServerApplication {
